c4266bdb9e125f472002d3a675abfdd3f022a4f1
MJJ <m3k0813@gmail.com>
2024-03-24 18:16:43 +0900

    feat: 비밀번호 암호화 저장

---
+++ src/main/java/com/techeer/abandoneddog/users/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.techeer.abandoneddog.users.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity
+                .httpBasic().disable()
+                .csrf().disable()
+                .cors().and()
+                .authorizeRequests()
+                .requestMatchers("/api/v1/users/login", "/api/v1/users/register").permitAll()
+                .and()
+                .build();
+    }
+}
--- src/main/java/com/techeer/abandoneddog/users/service/UserService.java
+++ src/main/java/com/techeer/abandoneddog/users/service/UserService.java
@@ -3,18 +3,25 @@ package com.techeer.abandoneddog.users.service;
 import com.techeer.abandoneddog.users.dto.UserDto;
 import com.techeer.abandoneddog.users.entity.Users;
 import com.techeer.abandoneddog.users.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class UserService {
     @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    private final BCryptPasswordEncoder encoder;
 
     public boolean signup(UserDto userDto) {
         Users users = new Users();
         users.setUsername(userDto.getUsername());
-        users.setPassword(userDto.getPassword());
+        users.setPassword(encoder.encode(userDto.getPassword()));
         users.setEmail(userDto.getEmail());
         users.setPhoneNum(userDto.getPhoneNum());
 
@@ -25,4 +32,6 @@ public class UserService {
             return false;
         }
     }
+
+
 }
