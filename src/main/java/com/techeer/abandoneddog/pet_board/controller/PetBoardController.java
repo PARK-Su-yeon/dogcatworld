197438d7b262db7919a3c1c44fa8bedc83ce960d
LeeJiWon <dlwldnjs1009@gmail.com>
2024-03-24 15:20:45 +0900

    feat: "추가 pet_board get api"

--- src/main/java/com/techeer/abandoneddog/pet_board/controller/PetBoardController.java
+++ src/main/java/com/techeer/abandoneddog/pet_board/controller/PetBoardController.java
@@ -3,9 +3,15 @@ package com.techeer.abandoneddog.pet_board.controller;
 import com.techeer.abandoneddog.pet_board.dto.PetBoardRequestDto;
 import com.techeer.abandoneddog.pet_board.dto.PetBoardResponseDto;
 import com.techeer.abandoneddog.pet_board.service.PetBoardService;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -39,6 +45,34 @@ public class PetBoardController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("반려동물 게시판 글 업데이트에 실패했습니다.");
+        }
+    }
+
+    @GetMapping("/{petBoardId}")
+    public ResponseEntity<?> getPetBoard(@PathVariable Long petBoardId) {
+        try {
+            PetBoardResponseDto responseDto = petBoardService.getPetBoard(petBoardId);
+            return ResponseEntity.ok(responseDto);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("게시글 조회에 실패하였습니다.");
+        }
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<?> getPetBoards(Pageable pageable) {
+        try {
+            Page<PetBoardResponseDto> petBoardPage = petBoardService.getPetBoards(pageable);
+            Map<String, Object> response = new LinkedHashMap<>();
+            response.put("message", "입양/분양 공고 리스트 조회 성공");
+            response.put("result", petBoardPage.getContent());
+
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            Map<String, String> errorResponse = new LinkedHashMap<>();
+            errorResponse.put("message", "입양/분양 공고 리스트 조회에 실패했습니다.");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
         }
     }
 
--- src/main/java/com/techeer/abandoneddog/pet_board/service/PetBoardService.java
+++ src/main/java/com/techeer/abandoneddog/pet_board/service/PetBoardService.java
@@ -6,6 +6,8 @@ import com.techeer.abandoneddog.pet_board.entity.PetBoard;
 import com.techeer.abandoneddog.pet_board.repository.PetBoardRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -27,4 +29,16 @@ public class PetBoardService {
         return PetBoardResponseDto.fromEntity(petBoard);
     }
 
+    @Transactional
+    public PetBoardResponseDto getPetBoard(Long petBoardId) {
+        PetBoard petBoard = petBoardRepository.findById(petBoardId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글을 찾을 수 없습니다. id=" + petBoardId));
+        return PetBoardResponseDto.fromEntity(petBoard);
+    }
+
+    @Transactional
+    public Page<PetBoardResponseDto> getPetBoards(Pageable pageable) {
+        Page<PetBoard> petBoardPage = petBoardRepository.findAll(pageable);
+        return petBoardPage.map(PetBoardResponseDto::fromEntity);
+    }
 }
