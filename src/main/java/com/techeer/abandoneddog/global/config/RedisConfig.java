86bfec1b60994c086592ad383b09a2dd6f7e93d6
수연 <syeon0131@naver.com>
2024-05-16 22:16:38 +0900

    feat: 캐싱

---
+++ src/main/java/com/techeer/abandoneddog/global/config/RedisConfig.java
@@ -0,0 +1,31 @@
+package com.techeer.abandoneddog.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofHours(24)))
+                .build();
+    }
+}
--- src/main/java/com/techeer/abandoneddog/shelter/service/ShelterService.java
+++ src/main/java/com/techeer/abandoneddog/shelter/service/ShelterService.java
@@ -1,17 +1,21 @@
 package com.techeer.abandoneddog.shelter.service;
 
-
 import com.techeer.abandoneddog.shelter.Dto.Coordinate;
 import com.techeer.abandoneddog.shelter.Dto.ShelterInfo;
 import com.techeer.abandoneddog.shelter.entity.Shelter;
 import com.techeer.abandoneddog.shelter.repository.ShelterRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.json.simple.JSONObject;
+
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
 
+import org.springframework.beans.factory.annotation.Value;
+
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.cache.annotation.Cacheable;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -20,17 +24,23 @@ import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 @Service
 @Slf4j
 public class ShelterService {
-    @Value("${GET_COORINATE}")
+    @Value("${GET_COORDINATE}")
     private String secretKey;
     private final ShelterRepository shelterRepository;
+
+    public RedisTemplate<String, String> redisTemplate;
 
     public ShelterService(ShelterRepository shelterRepository) {
         this.shelterRepository = shelterRepository;
     }
 
+
+     @Cacheable(value = "shelterCoordinates", key = "#address + ':' + #searchType")
     public List<ShelterInfo> getAllShelterInfos() {
         List<Shelter> shelters;
         try {
@@ -45,7 +55,13 @@ public class ShelterService {
         for (Shelter shelter : shelters) {
             try {
                 String address = shelter.getCareAddr();
+                log.error("11111111111111111111111111 shelter: {}");
                 Coordinate coordinate = getCoordinates(address, "ROAD");
+                log.error("222222222222222222222222222222222 shelter: {}");
+                cacheShelterCoordinates(shelter);
+                log.error("333333333333333333333333333 shelter: {}");
+
+
                 if (coordinate != null) {
                     ShelterInfo shelterInfo = new ShelterInfo(shelter.getCareNm(), coordinate);
                     shelterInfos.add(shelterInfo);
@@ -62,7 +78,7 @@ public class ShelterService {
     public Coordinate getCoordinates(String address, String searchType) {
         String apikey = secretKey;
         String epsg = "epsg:4326";
-
+        log.error("44444444444444444444444444444444444444444444 shelter: {}");
         try {
             StringBuilder sb = new StringBuilder("https://api.vworld.kr/req/address");
             sb.append("?service=address");
@@ -79,7 +95,7 @@ public class ShelterService {
             JSONObject jsob = (JSONObject) jspa.parse(reader);
             JSONObject jsrs = (JSONObject) jsob.get("response");
             JSONObject jsResult = (JSONObject) jsrs.get("result");
-
+            log.error("55555555555555555555555555555555555555 shelter: {}");
             if (jsResult == null) {
                 if ("ROAD".equals(searchType)) {
                     return getCoordinates(address, "PARCEL");
@@ -87,7 +103,7 @@ public class ShelterService {
                     return null;
                 }
             }
-
+            log.error("6666666666666666666666666666666666666666666666666 shelter: {}");
             JSONObject jspoint = (JSONObject) jsResult.get("point");
             double x = Double.parseDouble(jspoint.get("x").toString());
             double y = Double.parseDouble(jspoint.get("y").toString());
@@ -96,6 +112,31 @@ public class ShelterService {
         } catch (IOException | ParseException e) {
             log.error("Error occurred while getting coordinates: {}", e.getMessage());
             throw new RuntimeException("Error occurred while getting coordinates", e);
+        }
+    }
+
+
+    public void cacheShelterCoordinates(Shelter shelter) {
+        String cacheKey = "shelter:" + shelter.getCareNm();
+        String newCacheValue = null;
+        log.error("7777777777777777777777777777777777777 shelter: {}");
+        try {
+            Coordinate coordinate = getCoordinates(shelter.getCareAddr(), "ROAD");
+            if (coordinate != null) {
+                log.error("8888888888888888888888888888888 shelter: {}");
+                newCacheValue = "{\"latitude\": " + coordinate.getLatitude() + ", \"longitude\": " + coordinate.getLongitude() + "}";
+            }
+        } catch (Exception e) {
+            log.error("Error cache while getting coordinates: {}", e.getMessage());
+            // 예외 처리
+        }
+
+        if (newCacheValue != null) {
+            log.error("99999999999999999999999999999999999 shelter: {}");
+            redisTemplate.opsForValue().set(cacheKey, newCacheValue);
+            log.error("8887878787978798 shelter: {}");
+            redisTemplate.expire(cacheKey, 24, TimeUnit.HOURS); // 캐시 만료 시간 설정 (예: 24시간)
+            // 로깅 추가
         }
     }
 }
