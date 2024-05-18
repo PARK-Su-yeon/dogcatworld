6f3ab11d86dca69a22f7b27c4c1a72f7de4a50f1
LeeJiWon <dlwldnjs1009@gmail.com>
2024-05-19 00:13:03 +0900

    feat: 채팅 컨트롤러 구현

---
+++ src/main/java/com/techeer/abandoneddog/chatting/controller/WebSocketMessageController.java
@@ -0,0 +1,33 @@
+package com.techeer.abandoneddog.chatting.controller;
+
+import com.techeer.abandoneddog.chatting.dto.SimplifiedMessageDto;
+import com.techeer.abandoneddog.chatting.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketMessageController {
+
+    private final ChatService chatService;
+
+    @MessageMapping("/message")
+    public void handleMessage(SimplifiedMessageDto messageDto) {
+        switch (messageDto.getType()) {
+            case ENTER:
+                chatService.enterChatRoom(messageDto);
+                break;
+            case TALK:
+                chatService.sendMessageToChatRoom(messageDto);
+                break;
+            case LEAVE:
+                chatService.leaveChatRoom(messageDto.getSenderId(), messageDto.getChatRoomId());
+                break;
+            default:
+                log.warn("Unknown message type: {}", messageDto.getType());
+        }
+    }
+}
