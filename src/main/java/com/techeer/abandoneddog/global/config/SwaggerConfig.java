cc4550bac92786661c8f3b27a4431428118777ba
수연 <syeon0131@naver.com>
2024-06-06 23:40:21 +0900

    feat: swagger 인증버튼 추가

--- src/main/java/com/techeer/abandoneddog/global/config/SwaggerConfig.java
+++ src/main/java/com/techeer/abandoneddog/global/config/SwaggerConfig.java
@@ -1,43 +1,37 @@
-//package com.techeer.abandoneddog.global.config;
-//
-//import io.swagger.v3.oas.models.Components;
-//import io.swagger.v3.oas.models.OpenAPI;
-//import io.swagger.v3.oas.models.info.Info;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.core.context.SecurityContext;
-//
-//import java.util.List;
-//
-//@Configuration    // 스프링 실행시 설정파일 읽어드리기 위한 어노테이션
-//public class SwaggerConfig {
-//
-//    @Bean
-//    public OpenAPI openAPI() {
-//        return new OpenAPI()
-//                .components(new Components())
-//                .info(apiInfo());
-//    }
-//
-//    private Info apiInfo() {
-//        return new Info()
-//                .title("CodeArena Swagger")
-//                .description("CodeArena 유저 및 인증 , ps, 알림에 관한 REST API")
-//                .version("1.0.0")
-//                .securityContexts(List.of(this.securityContext())) // SecurityContext 설정
-//                .securitySchemes(List.of(this.apiKey())) // ApiKey 설정
-//    }
-//    private SecurityContext securityContext() {
-//        return SecurityContext.builder()
-//                .securityReferences(defaultAuth())
-//                .build();
-//    }
-//
-//    private List<SecurityReference> defaultAuth() {
-//        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
-//        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-//        authorizationScopes[0] = authorizationScope;
-//        return List.of(new SecurityReference("Authorization", authorizationScopes));
-//    }
-//
-//}
+package com.techeer.abandoneddog.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("Pet API")
+                .description("API for Pet Service");
+
+        String apiKeySchemeName = "ApiKey Authentication";
+
+        // 요청 헤더에 인증 정보 포함
+        SecurityRequirement securityItem = new SecurityRequirement().addList(apiKeySchemeName);
+
+        Components components = new Components()
+                .addSecuritySchemes(apiKeySchemeName, new io.swagger.v3.oas.models.security.SecurityScheme()
+                        .name(apiKeySchemeName)
+                        .type(SecurityScheme.Type.APIKEY)
+                        .in(SecurityScheme.In.HEADER)
+                        .name("access"));
+
+        return new OpenAPI()
+                .info(info)
+                .addSecurityItem(securityItem)
+                .components(components);
+    }
+}
