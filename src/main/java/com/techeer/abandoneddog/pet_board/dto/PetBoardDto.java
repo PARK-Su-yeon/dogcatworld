5cea9eefa0c174a7ada2012728d6496f9db81761
MJJ <m3k0813@gmail.com>
2024-06-16 19:39:31 +0900

    feat: 북마크 api response 수정

---
+++ src/main/java/com/techeer/abandoneddog/pet_board/dto/PetBoardDto.java
@@ -0,0 +1,23 @@
+package com.techeer.abandoneddog.pet_board.dto;
+
+import com.techeer.abandoneddog.pet_board.entity.PetBoard;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PetBoardDto {
+    private Long petBoardId;
+    private String title;
+    private String popfile;
+
+    public static PetBoardDto fromEntity(PetBoard petBoard) {
+        return new PetBoardDto(
+                petBoard.getPetBoardId(),
+                petBoard.getTitle(),
+                petBoard.getPetInfo() != null ? petBoard.getPetInfo().getPopfile() : null
+        );
+    }
+}
