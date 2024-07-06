0e69273397932d7d07141c24c44da5e2b3a24854
수연 <syeon0131@naver.com>
2024-07-06 21:59:42 +0900

    fix:상세조회 수정

--- src/main/java/com/techeer/abandoneddog/pet_board/dto/PetBoardDetailResponseDto.java
+++ src/main/java/com/techeer/abandoneddog/pet_board/dto/PetBoardDetailResponseDto.java
@@ -1,11 +1,15 @@
 package com.techeer.abandoneddog.pet_board.dto;
 
 import com.techeer.abandoneddog.animal.PetInfoDto.PetInfoResponseDto;
+import com.techeer.abandoneddog.image.entity.Image;
 import com.techeer.abandoneddog.pet_board.entity.PetBoard;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.lang.Nullable;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @AllArgsConstructor
@@ -20,12 +24,20 @@ public class PetBoardDetailResponseDto {
     private String status;
     private boolean isLiked;
     private Integer likeCount;
+    private List<String> imageUrls;
 
     public static PetBoardDetailResponseDto fromEntity(PetBoard petBoard, boolean isLiked) {
         PetInfoResponseDto petInfoResponseDto = null;
         if (petBoard.getPetInfo() != null) {
             petInfoResponseDto = PetInfoResponseDto.fromEntity(petBoard.getPetInfo());
         }
+
+
+        List<String> imageUrls = petBoard.getPetInfo() != null && petBoard.getPetInfo().getImages() != null
+                ? petBoard.getPetInfo().getImages().stream()
+                .map(Image::getUrl)
+                .collect(Collectors.toList())
+                : List.of();
 
         return new PetBoardDetailResponseDto(
                 petBoard.getPetBoardId(),
@@ -34,7 +46,8 @@ public class PetBoardDetailResponseDto {
                 petInfoResponseDto,
                 petBoard.getStatus() != null ? petBoard.getStatus().toString() : "N/A",
                 isLiked,
-                petBoard.getLikeCount()
+                petBoard.getLikeCount(),
+                imageUrls
         );
     }
 }
