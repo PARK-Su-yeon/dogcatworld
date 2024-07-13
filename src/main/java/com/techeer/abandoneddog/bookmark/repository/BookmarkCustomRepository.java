fa62f1621468351222c1f7cb10ac52f630ba32a0
MJJ <m3k0813@gmail.com>
2024-07-13 18:17:56 +0900

    feat: 북마크 조회 querydls 적용

--- src/main/java/com/techeer/abandoneddog/bookmark/repository/BookmarkCustomRepository.java
+++ src/main/java/com/techeer/abandoneddog/bookmark/repository/BookmarkCustomRepository.java
@@ -4,9 +4,6 @@ import com.techeer.abandoneddog.bookmark.entity.Bookmark;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
-
 public interface BookmarkCustomRepository {
     Page<Bookmark> findBookmarksByUserIdAndIsDeletedFalse(Pageable pageable, Long userId);
-    List<Bookmark> findBookmarkListByUserIdAndIsDeletedFalse(Long userId);
 }
--- src/main/java/com/techeer/abandoneddog/bookmark/repository/BookmarkCustomRepositoryImpl.java
+++ src/main/java/com/techeer/abandoneddog/bookmark/repository/BookmarkCustomRepositoryImpl.java
@@ -5,8 +5,8 @@ import com.techeer.abandoneddog.animal.entity.QPetInfo;
 import com.techeer.abandoneddog.bookmark.entity.Bookmark;
 import com.techeer.abandoneddog.bookmark.entity.QBookmark;
 import com.techeer.abandoneddog.pet_board.entity.QPetBoard;
-import com.techeer.abandoneddog.users.entity.QUsers;
 import com.techeer.abandoneddog.shelter.entity.QShelter;
+import com.techeer.abandoneddog.users.entity.QUsers;
 import jakarta.transaction.Transactional;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -59,25 +59,5 @@ public class BookmarkCustomRepositoryImpl implements BookmarkCustomRepository {
         .fetchCount();
 
     return new PageImpl<>(bookmarks, pageable, total);
-  }
-
-  @Override
-  public List<Bookmark> findBookmarkListByUserIdAndIsDeletedFalse(Long userId) {
-    QBookmark bookmark = QBookmark.bookmark;
-    QPetBoard petBoard = QPetBoard.petBoard;
-    QPetInfo petInfo = QPetInfo.petInfo;
-    QUsers user = QUsers.users;
-    QShelter shelter = QShelter.shelter;
-
-    return queryFactory.selectFrom(bookmark)
-        .leftJoin(bookmark.petBoard, petBoard).fetchJoin()
-        .leftJoin(petBoard.petInfo, petInfo).fetchJoin()
-        .leftJoin(petBoard.users, user).fetchJoin()
-        .leftJoin(petInfo.shelter, shelter).fetchJoin()
-        .where(bookmark.user.id.eq(userId)
-            .and(bookmark.isDeleted.eq(false))
-            .and(petBoard.isDeleted.eq(false))
-            .and(petInfo.isDeleted.eq(false)))
-        .fetch();
   }
 }
