7fb66f195edd707ff69a72f258da98c4c8aed971
MJJ <m3k0813@gmail.com>
2024-07-13 17:31:12 +0900

    feat: 북마크 조회 querydls 적용

--- src/main/java/com/techeer/abandoneddog/bookmark/repository/BookmarkRepository.java
+++ src/main/java/com/techeer/abandoneddog/bookmark/repository/BookmarkRepository.java
@@ -7,22 +7,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
-
-public interface BookmarkRepository extends JpaRepository<Bookmark, Long>, BookmarkCustomRepository {
+public interface BookmarkRepository extends JpaRepository<Bookmark, Long>,
+    BookmarkCustomRepository {
 
 //    @Query("SELECT b FROM Bookmark b JOIN FETCH b.petBoard WHERE b.user.id = :userId AND b.isDeleted = false")
 //    Page<Bookmark> findBookmarksByUserIdAndIsDeletedFalse(Pageable pageable,@Param("userId") Long userId);
 
-    boolean existsByPetBoardAndUser(PetBoard petBoard, Users user);
+  boolean existsByPetBoardAndUser(PetBoard petBoard, Users user);
 
-    boolean existsByPetBoardAndUserIdAndIsDeletedFalse(PetBoard petBoard, Long userId);
+  boolean existsByPetBoardAndUserIdAndIsDeletedFalse(PetBoard petBoard, Long userId);
 
-    Bookmark findByPetBoardAndUser(PetBoard petBoard, Users users);
+  Bookmark findByPetBoardAndUser(PetBoard petBoard, Users users);
 
-    List<Bookmark> findAllByUserAndIsDeletedFalse(Users user);
-
-    @Modifying
-    @Query("UPDATE Bookmark b SET b.isDeleted=false WHERE b.id= :bookmarkId")
-    void resave(Long bookmarkId);
+  @Modifying
+  @Query("UPDATE Bookmark b SET b.isDeleted=false WHERE b.id= :bookmarkId")
+  void resave(Long bookmarkId);
 }
